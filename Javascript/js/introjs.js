cf4f4813b01606777945caa6deb74654b5563665
alexanderavila16 <simplyalexander16@gmail.com>
2021-07-20 12:45:42 -0500

    archivos finales javascript

---
+++ Javascript/js/introjs.js
@@ -0,0 +1,16 @@
+//alerta en javascript
+// comentario multilinea 
+
+// alert("hola mundo");
+// alert("hola mundo 2");
+
+//Consola en JS
+// console.log('hola saludamos a la consola');
+// console.log(2+2);
+// console.info('mostrar una informacion');
+// console.warn('estamos en una advvertencia');
+// console.error('mostrar un error');
+
+//Document.write
+// voy acceder a mi documnto y voy a escribir en el en el html
+document.write('hola este mensaje se mostrara en mi web');
